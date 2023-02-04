70d79116a4a85b44ec5b8a214321e872e9f91c38
Nga Ho <ngahtpps25336@fpt.edu.vn>
2023-02-04 21:26:45 +0700

    first commit

--- jira-project/src/app.tsx
+++ jira-project/src/app.tsx
@@ -1,30 +1,28 @@
-import { Route, Routes, unstable_HistoryRouter as HistoryBrowser, } from 'react-router-dom'
-import { createBrowserHistory } from 'history';
+import {
+  Route,
+  Routes,
+  unstable_HistoryRouter as HistoryBrowser,
+} from "react-router-dom";
+import { createBrowserHistory } from "history";
 
 import { Provider } from "react-redux";
-import HomeTemplate from './templates/home-template';
-import { store } from './redux/configStore';
-import Login from './Pages/Login/Login';
-import CreateProject from './Pages/CreateProject/CreateProject';
-import { PageConstant } from './commom/page.constant';
-import Project from './Pages/Project/Project';
+import HomeTemplate from "./templates/home-template";
+import { store } from "./redux/configStore";
+import { PageConstant } from "./commom/page.constant";
+import Project from "./Pages/Project/Project";
 
-export const history: any = createBrowserHistory()
+export const history: any = createBrowserHistory();
 
 export default function App() {
-    return (
-        <Provider store={store}>
-            <HistoryBrowser history={history}>
-                <Routes>
-                    
-                    <Route path='/login' element={<Login />}/>
-                        <Route path='/' element={<HomeTemplate />}>
-                        <Route path={`${PageConstant.project}`} element={<Project />}></Route>
-                            <Route path='/createProject' element={<CreateProject />}/>
-                        </Route>
-                </Routes>
-            </HistoryBrowser>
-        </Provider>
-        
-    )
+  return (
+    <Provider store={store}>
+      <HistoryBrowser history={history}>
+        <Routes>
+          <Route path="/" element={<HomeTemplate />}>
+            <Route path={`${PageConstant.project}`} element={<Project />}></Route>
+          </Route>
+        </Routes>
+      </HistoryBrowser>
+    </Provider>
+  );
 }
