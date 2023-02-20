656764d2d1ce7f8b210fbcaf3ece254180a8411f
dvphuc96 <phuc.dovan.96@gmail.com>
2023-02-20 11:05:57 +0700

    update file deploy

--- jira-project/deploy-product.sh
+++ jira-project/deploy-product.sh
@@ -1,11 +1,14 @@
-# 1.Build reactjs app with production mode
+# 1.Remove the build folder (if any)
+rm -rf build
+
+# 2.Build reactjs app with production mode
 npm run build
 
-# 2.Move to build folder
+# 3.Move to build folder
 cd build
 
-# 3.Clone file index.html into 200.html
+# 4.Clone file index.html into 200.html
 cp index.html 200.html
 
-#4 Deploy
+# 5.Deploy
 npx surge . jira-project-app.surge.sh
